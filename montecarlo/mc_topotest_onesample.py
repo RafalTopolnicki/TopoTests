d86469a5645606e2467cfdb1b956a23d8b4b5e9b
RafalTopolnicki <rafal.topolnicki@gmail.com>
2022-06-20 21:08:05 +0200

    TopoTest-1s is now able to continue calculations

--- montecarlo/mc_topotest_onesample.py
+++ montecarlo/mc_topotest_onesample.py
@@ -38,9 +38,24 @@ def run_mc(rvs, args):
     outputfilename = f"dim={args.dim}_n={args.n}_norm={args.norm}_method={args.method}.csv"
     outputfilepath = os.path.join(args.output_dp, outputfilename)
     results = []
+    df_done = None
+    if os.path.exists(outputfilepath):
+        logging.info(f"OUTPUTFILE FOUND! Will continue from here.")
+        df_done = pd.read_csv(outputfilepath).iloc[:, 1:]
+        outputfilepath = os.path.join(args.output_dp, outputfilename+'.cont') #fixme: this works only for one restart
 
     for rv_true in rvs:
         logging.info(f"TT-1s: Start true distribution: {rv_true.label} n={args.n} dim={args.dim} norm={args.norm}")
+        rv_true_done = True
+        for rv_alter in rvs:
+            if df_done is not None and not np.any(np.logical_and(df_done.true_dist == rv_true.label, df_done.alter_dist == rv_alter.label)):
+                #at least one alternative distribution is missing - need to repeat whole simulation
+                #for that true distribution
+                rv_true_done = False
+        if rv_true_done:
+            logging.info(f"TT-1s: All with true distribution: {rv_true.label} n={args.n} dim={args.dim} norm={args.norm} done!")
+            continue
+
         topo_test = TopoTest_onesample(
             n=args.n,
             dim=args.dim,
@@ -51,24 +66,28 @@ def run_mc(rvs, args):
         # train TopoTest
         topo_test.fit(rv=rv_true, n_signature=args.n_signature, n_test=args.n_test)
         for rv_alter in rvs:
-            logging.info(f"TT-1s: Start distribution true: {rv_true.label} alter: {rv_alter.label}")
-            # generate samples from alternative distributions
-            samples = [rv_alter.rvs(args.n) for i in range(args.M)]
-            # get list of H0 acceptances and p-values
-            tt_out = topo_test.predict(samples)
-            results.append(
-                data_row(
-                    args=args,
-                    true_label=rv_true.label,
-                    alter_label=rv_alter.label,
-                    accpecth0=tt_out[0],
-                    pvals=tt_out[1],
-                    threshold=topo_test.representation_threshold,
+            if df_done is not None and np.any(np.logical_and(df_done.true_dist == rv_true.label, df_done.alter_dist == rv_alter.label)):
+                logging.info(f"TT-1s: Skip distribution true: {rv_true.label} alter: {rv_alter.label}")
+                continue
+            else:
+                logging.info(f"TT-1s: Start distribution true: {rv_true.label} alter: {rv_alter.label}")
+                # generate samples from alternative distributions
+                samples = [rv_alter.rvs(args.n) for i in range(args.M)]
+                # get list of H0 acceptances and p-values
+                tt_out = topo_test.predict(samples)
+                results.append(
+                    data_row(
+                        args=args,
+                        true_label=rv_true.label,
+                        alter_label=rv_alter.label,
+                        accpecth0=tt_out[0],
+                        pvals=tt_out[1],
+                        threshold=topo_test.representation_threshold,
+                    )
                 )
-            )
-            # write results to file after each test
-            df = pd.DataFrame(results)
-            df.to_csv(outputfilepath)
+                # write results to file after each test
+                df_data = pd.DataFrame(results)
+                df_data.to_csv(outputfilepath)
 
 
 def main():
