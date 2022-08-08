07f1dfc357e55fa8d425d8ac30dd5addcf7c6830
RafalTopolnicki <rafal.topolnicki@gmail.com>
2022-08-08 21:04:07 +0200

    3d add

--- montecarlo/randomvariables.py
+++ montecarlo/randomvariables.py
@@ -57,30 +57,47 @@ def get_random_variables(dim):
     if dim == 2:
         rvs = [
             MultivariateDistribution([st.norm(), st.norm()], label="N01xN01"),
+            MultivariateGaussian(dim=2, a=0.05, label="MultiGauss0.05"),
             MultivariateGaussian(dim=2, a=0.1, label="MultiGauss0.1"),
+            MultivariateGaussian(dim=2, a=0.2, label="MultiGauss0.2"),
+            MultivariateGaussian(dim=2, a=0.3, label="MultiGauss0.3"),
             MultivariateGaussian(dim=2, a=0.5, label="MultiGauss0.5"),
-            MultivariateGaussian(dim=2, a=0.9, label="MultiGauss0.9"),
             MultivariateDistribution([st.t(df=3), st.t(df=3)], label="T3xT3"),
             MultivariateDistribution([st.t(df=5), st.t(df=5)], label="T5xT5"),
             MultivariateDistribution([st.t(df=10), st.t(df=10)], label="T10xT10"),
+            MultivariateDistribution([st.t(df=25), st.t(df=25)], label="T25x25"),
             MultivariateDistribution([st.logistic(), st.logistic()], label="LogisticxLogistic"),
             MultivariateDistribution([st.laplace(), st.laplace()], label="LaplacexLaplace"),
+            MultivariateDistribution([st.norm(), st.t(df=3)], label="N01xT3"),
             MultivariateDistribution([st.norm(), st.t(df=5)], label="N01xT5"),
+            MultivariateDistribution([st.norm(), st.t(df=10)], label="N01xT10"),
             MultivariateDistribution(
-                [GaussianMixture([-1, 1], [1, 1], [0.5, 0.5]), GaussianMixture([-1, 1], [1, 1], [0.5, 0.5])],
-                label="GM_1xGM_1",
+                [GaussianMixture([0, 1], [1, 0.5], [0.9, 0.1]),
+                 GaussianMixture([0, 1], [1, 0.5], [0.9, 0.1])],
+                label="GM_1xGM_1"
             ),
-            MultivariateDistribution([st.norm(), GaussianMixture([-1, 1], [1, 1], [0.5, 0.5])], label="N01xGM_1"),
+            MultivariateDistribution(
+                [GaussianMixture([0, 1], [1, 0.5], [0.7, 0.3]),
+                 GaussianMixture([0, 1], [1, 0.5], [0.7, 0.3])],
+                label="GM_2xGM_2"
+            ),
+            MultivariateDistribution(
+                [GaussianMixture([0, 1], [1, 0.5], [0.5, 0.5]),
+                 GaussianMixture([0, 1], [1, 0.5], [0.5, 0.5])],
+                label="GM_3xGM_3"
+            )
         ]
     if dim == 3:
         rvs = [
             MultivariateDistribution([st.norm(), st.norm(), st.norm()], label="N01xN01xN01"),
+            MultivariateGaussian(dim=3, a=0.05, label="MultiGauss0.05"),
             MultivariateGaussian(dim=3, a=0.1, label="MultiGauss0.1"),
+            MultivariateGaussian(dim=3, a=0.2, label="MultiGauss0.2"),
+            MultivariateGaussian(dim=3, a=0.3, label="MultiGauss0.3"),
             MultivariateGaussian(dim=3, a=0.5, label="MultiGauss0.5"),
-            MultivariateGaussian(dim=3, a=0.9, label="MultiGauss0.9"),
-            MultivariateDistribution([st.t(df=3), st.t(df=3), st.t(df=3)], label="T3xT3xT3"),
             MultivariateDistribution([st.uniform(), st.uniform(), st.uniform()], label="UxUxU"),
             MultivariateDistribution([st.beta(2, 2), st.beta(2, 2), st.beta(2, 2)], label="B22xB22xB22"),
+            MultivariateDistribution([st.t(df=3), st.t(df=3), st.t(df=3)], label="T3xT3xT3"),
             MultivariateDistribution([st.t(df=5), st.t(df=5), st.t(df=5)], label="T5xT5xT5"),
             MultivariateDistribution([st.t(df=10), st.t(df=10), st.t(df=10)], label="T10xT10xT10"),
             MultivariateDistribution([st.logistic(), st.logistic(), st.logistic()], label="LogisticxLogisticxLogistic"),
@@ -88,13 +105,23 @@ def get_random_variables(dim):
             MultivariateDistribution([st.norm(), st.t(df=5), st.t(df=5)], label="N01xT5xT5"),
             MultivariateDistribution([st.norm(), st.norm(), st.t(df=5)], label="N01xN01xT5"),
             MultivariateDistribution(
-                [
-                    GaussianMixture([-1, 1, 0], [1, 1, 1], [0.33, 0.33, 0.34]),
-                    GaussianMixture([-1, 1, 0], [1, 1, 1], [0.33, 0.33, 0.34]),
-                    GaussianMixture([-1, 1, 0], [1, 1, 2], [0.33, 0.33, 0.34]),
-                ],
-                label="GM1",
+                [GaussianMixture([0, 1], [1, 0.5], [0.9, 0.1]),
+                 GaussianMixture([0, 1], [1, 0.5], [0.9, 0.1]),
+                 GaussianMixture([0, 1], [1, 0.5], [0.9, 0.1])],
+                label="GM_1xGM_1xGM_1"
             ),
+            MultivariateDistribution(
+                [GaussianMixture([0, 1], [1, 0.5], [0.7, 0.3]),
+                 GaussianMixture([0, 1], [1, 0.5], [0.7, 0.3]),
+                 GaussianMixture([0, 1], [1, 0.5], [0.7, 0.3])],
+                label="GM_2xGM_2xGM_2"
+            ),
+            MultivariateDistribution(
+                [GaussianMixture([0, 1], [1, 0.5], [0.5, 0.5]),
+                 GaussianMixture([0, 1], [1, 0.5], [0.5, 0.5]),
+                 GaussianMixture([0, 1], [1, 0.5], [0.5, 0.5])],
+                label="GM_3xGM_3xGM_3"
+            )
         ]
     if dim == 5:
         rvs = [
