89d929fd256ded07275aea176257660a104ccc17
RafalTopolnicki <rafal.topolnicki@gmail.com>
2023-05-21 11:52:48 +0200

    PDLL test

---
+++ topotests/pdll.py
@@ -0,0 +1,46 @@
+import numpy as np
+import scipy.interpolate as spi
+from scipy.spatial import distance_matrix
+import gudhi as gd
+import random
+from statsmodels.distributions.empirical_distribution import ECDF
+
+def normal_density(x, loc, sigma):
+    return np.exp(-(x-loc)**2/(2*sigma))
+
+class pdll_representation:
+    def __init__(self, persistence_dim, sigma):
+        self.pdpoints = None
+        self.n_fitted = 0
+        self.fitted = False
+        self.persistence_dim = persistence_dim
+        self.sigma = sigma
+
+    def _compute_persistance_points(self, samples):
+        all_pers_points = None
+        for sample_id, sample in enumerate(samples):
+            ac = gd.AlphaComplex(points=sample).create_simplex_tree()
+            ac.compute_persistence()
+            pers_points = ac.persistence_intervals_in_dimension(self.persistence_dim)
+            if sample_id == 0:
+                all_pers_points = pers_points
+            else:
+                all_pers_points = np.vstack((all_pers_points, pers_points))
+        return all_pers_points
+
+    def fit(self, samples):
+        self.pdpoints = self._compute_persistance_points(samples)
+        self.fitted = True
+    def transform(self, samples):
+        pdpoints = self._compute_persistance_points(samples)
+        return pdpoints
+
+    def get_ll(self, sample):
+        pdpoints = self.transform([sample])
+        loglikelihood = 0
+        for pdpoint in pdpoints:
+            pdpoints_density = normal_density(x=pdpoint, loc=self.pdpoints, sigma=self.sigma)
+            # since MVN has diagonal covarinace matrix we can total density is just a product of densities in each axis
+            loglikelihood += np.log(np.sum(np.prod(pdpoints_density, axis=1)))
+        return loglikelihood
+
---
+++ topotests/pdlltest.py
@@ -0,0 +1,64 @@
+import numpy as np
+from pdll import pdll_representation
+
+
+def sample_standarize(sample):
+    return (sample - np.mean(sample, axis=0)) / np.std(sample, axis=0)
+
+
+class PDLLTest_onesample:
+    def __init__(
+        self,
+        n: int,
+        dim: int,
+        persistence_dim: int,
+        sigma: float,
+        significance_level: float = 0.05,
+        scaling=1,
+        standarize=False,
+    ):
+        """
+        TODO: add comment
+        """
+        self.fitted = False
+        self.sample_pts_n = n
+        self.data_dim = dim
+        self.persistence_dim = persistence_dim
+        self.sigma = sigma,
+        self.significance_level = significance_level
+        self.standarize = standarize
+        self.scaling = scaling
+        self.representation = pdll_representation(persistence_dim=self.persistence_dim, sigma=self.sigma)
+        self.loglikelihoods = None
+        self.representation_threshold = None,
+
+    def fit(self, rv, n_signature, n_test):
+        # generate signature samples and test sample
+        samples = [rv.rvs(self.sample_pts_n) * self.scaling for i in range(n_signature)]
+        samples_test = [rv.rvs(self.sample_pts_n) * self.scaling for i in range(n_test)]
+        if self.standarize:
+            samples = [sample_standarize(sample) for sample in samples]
+            samples_test = [sample_standarize(sample) for sample in samples_test]
+
+        # get signatures representations of both samples
+        self.representation.fit(samples)
+        self.loglikelihoods = [self.representation.get_ll(sample=x) for x in samples_test]
+        self.representation_threshold = np.quantile(self.loglikelihoods, 1 - self.significance_level)
+        self.fitted = True
+
+    def predict(self, samples):
+        if not self.fitted:
+            raise RuntimeError("Cannot run predict(). Run fit() first!")
+        if len(samples) == 1:
+            samples = [samples]
+
+        samples = [sample * self.scaling for sample in samples]
+
+        if self.standarize:
+            samples = [sample_standarize(sample) for sample in samples]
+
+        ll_predict = [self.representation.get_ll(sample=sample) for sample in samples]
+        accpect_h0 = [ll < self.representation_threshold for ll in ll_predict]
+        # calculate pvalues
+        pvals = [np.mean(self.loglikelihoods > ll) for ll in ll_predict]
+        return accpect_h0, pvals
